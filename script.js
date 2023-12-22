5ecb26c3737cdaf4fa19055afe4c366740d5fba6
CherikchievTimur <95346435+CherikchievTimur@users.noreply.github.com>
2023-12-21 18:30:55 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+const dogsBreedList = document.querySelector(`#dogs-list`);
+const dogImage = document.querySelector('#dog');
+let dogBread = 'Any';
+
+const getDogsBreedList = async () => {
+    const res = await fetch(`https://dog.ceo/api/breeds/list/all`);
+    const data = await res.json()
+
+    return data;
+}
+
+const loadDogsBreedList = async () => {
+    const data = await getDogsBreedList();
+
+    for (let breed of Object.entries(data.message)) {
+        if (breed[1].length > 0) {
+            for (let subBreed of breed[1]) {
+                addDogBreed(`${breed[0]} ${subBreed}`);
+            }
+        }
+        else {
+            addDogBreed(`${breed[0]}`);
+        }
+    }
+}
+
+const addDogBreed = (breed) => {
+    const option = document.createElement(`option`);
+
+    option.value = breed;
+    option.textContent = breed;
+
+    dogsBreedList.append(option);
+}
+
+const getDogImage = async (breed) => {
+    let url = '';
+
+    if(breed === 'Any')
+        url = `https://dog.ceo/api/breeds/image/random`
+    else
+        url = `https://dog.ceo/api/breed/${breed}/images/random`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    dogImage.src = data.message;
+}
+
+const onChangeDogBreed = (e) => {
+    dogBread = e.target.value;
+    getDogImage(dogBread);
+}
+
+const onClickRefresh = () => {
+    getDogImage(dogBread);
+}
+
+loadDogsBreedList();
+getDogImage(dogBread);
